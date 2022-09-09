e5e1638aa66a0e7cda8805588b12e9371f152a15
Eddiebee <edbeeotudor@gmail.com>
2022-09-09 15:36:47 +0100

    Add User component to View

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Counter from "./features/counter/Counter";
+import Users from "./features/users/Users";
 
 function App() {
   return (
     <main className="App">
       <Counter />
+      <Users />
     </main>
   );
 }
