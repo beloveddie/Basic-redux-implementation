43a3dbdf7b226b03085003fe64d43fa587339d3b
Eddiebee <edbeeotudor@gmail.com>
2022-09-09 22:03:55 +0100

    Add userApi reducer and middleware

--- src/app/store.js
+++ src/app/store.js
@@ -4,5 +4,11 @@ import counterReducer from "../features/counter/counterSlice";
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    // Add the generated reducer as a specific top-level slice
+    [userApi.reducerPath]: userApi.reducer,
+  },
+  // Adding the Api middleware enables caching, invalidation, polling, and other useful features of rtk-query
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(userApi.middleware);
   },
 });
