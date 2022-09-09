bf692665aa5aa406ffb4090c56825d456d7ac93f
Eddiebee <edbeeotudor@gmail.com>
2022-09-09 15:36:33 +0100

    Add display fields for user data

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Users = () => {
-  return <div>Users</div>;
+  return (
+    <>
+      <input type="text" />
+
+      <section>
+        <header>
+          <h1>User Details</h1>
+        </header>
+        <div>
+          <ul>
+            <li>Name: Leane</li>
+            <li>Username: Bret</li>
+            <li>email: Leane</li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Users;
