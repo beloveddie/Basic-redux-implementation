0ada3bb53b778585452c134519d56603f633b5de
Eddiebee <edbeeotudor@gmail.com>
2022-09-09 21:12:41 +0100

    Build up interface

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -3,18 +3,35 @@ import React from "react";
 const Users = () => {
   return (
     <>
-      <input type="text" />
-
-      <section>
-        <header>
-          <h1>User Details</h1>
+      <section className="overflow-hidden bg-white shadow sm:rounded-lg">
+        <input
+          type="text"
+          placeholder="ID"
+          className="text-3xl text-gray-500"
+        />
+        <header className="px-4 py-5 sm:px-6">
+          <h1 className="text-lg font-medium leading-6 text-gray-900">
+            User Information
+          </h1>
+          <p className="mt-1 max-w-2xl text-sm text-gray-500">
+            Gotten from the{" "}
+            <a
+              href="https://jsonplaceholder.typicode.com/users"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              JSON placeholder API users endpoint
+            </a>
+          </p>
         </header>
-        <div>
-          <ul>
-            <li>Name: Leane</li>
-            <li>Username: Bret</li>
-            <li>email: Leane</li>
-          </ul>
+        <div className="border-t border-gray-200 w-full">
+          <dl>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Full name</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                Margot Foster
+              </dd>
+            </div>
+          </dl>
         </div>
       </section>
     </>
